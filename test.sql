070c23ea8d375f09d82328921c89fb91e7b4e060
Anthony-Suni <85529514+Anthony-Suni@users.noreply.github.com>
2023-12-14 20:39:49 -0500

    Update test.sql

--- test.sql
+++ test.sql
@@ -2,16 +2,16 @@
 CREATE DATABASE recomendaciones;
 
 -- Conectarse a la base de datos Recomendaciones
-\c Recomendaciones;
+\c recomendaciones;
 
 -- Crear la tabla users
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
-    genero VARCHAR(255), -- Asegúrate de que coincida con el nombre de la propiedad en tu modelo
-    pelicula VARCHAR(255), -- Asegúrate de que coincida con el nombre de la propiedad en tu modelo
-    rating INT -- Asegúrate de que coincida con el nombre de la propiedad en tu modelo
+    genero VARCHAR(255),
+    pelicula VARCHAR(255), 
+    rating INT 
 );
 
 
