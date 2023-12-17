f1722da3578d87e01c1b3eab7e6b74072fe8d62c
Anthony-Suni <85529514+Anthony-Suni@users.noreply.github.com>
2023-12-17 15:19:26 -0500

    Update test.sql

--- test.sql
+++ test.sql
@@ -14,5 +14,12 @@ CREATE TABLE users (
     rating INT 
 );
 
+-- Crear la tabla movies
+CREATE TABLE movies (
+    id SERIAL PRIMARY KEY,
+    pelicula VARCHAR(255) NOT NULL,
+    genero VARCHAR(255),
+    ratings INT[] -- Un array para almacenar múltiples ratings
+);
 
 -- Puedes agregar más comandos SQL según sea necesario para tu aplicación.
