061ad16978e05a0b0d958eb71dcce45200699df2
Anthony-Suni <85529514+Anthony-Suni@users.noreply.github.com>
2023-12-17 18:53:35 -0500

    Update UserContext.cs

--- Data/UserContext.cs
+++ Data/UserContext.cs
@@ -10,16 +10,11 @@ namespace Data
         }
 
         public DbSet<User> Users { get; set; }
-        public DbSet<Movie> Movies { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            // Puedes configurar aquí las relaciones, índices y otros detalles del modelo si es necesario.
-            // Aquí se muestra un ejemplo simple de configuración:
-            modelBuilder.Entity<User>()
-                .HasMany(u => u.Movies)
-                .WithOne()
-                .HasForeignKey(m => m.UserId); // Asume que hay una propiedad UserId en la clase Movie
+            // Configuraciones específicas de UserContext si es necesario
+            // ...
         }
     }
 }
