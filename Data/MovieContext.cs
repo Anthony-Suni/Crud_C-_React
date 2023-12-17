41f048b87b4ecc293d38c20486fac14ff70c5764
Anthony-Suni <85529514+Anthony-Suni@users.noreply.github.com>
2023-12-17 17:45:31 -0500

    Update MovieContext.cs

--- Data/MovieContext.cs
+++ Data/MovieContext.cs
@@ -1,1 +1,15 @@
+using Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data
+{
+    public class MovieContext : DbContext
+    {
+        public MovieContext(DbContextOptions<MovieContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
 
