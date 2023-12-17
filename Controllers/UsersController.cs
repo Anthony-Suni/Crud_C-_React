570184e327f586d6c84e5276f50e038bf2f23432
Anthony-Suni <85529514+Anthony-Suni@users.noreply.github.com>
2023-12-17 15:57:43 -0500

    Update UsersController.cs

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -2,100 +2,170 @@ using Data;
 using Microsoft.AspNetCore.Mvc;
 using Models;
 using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
-namespace csharp_crud_api.Controllers;
-
-[ApiController]
-[Route("api/[controller]")]
-public class UsersController : ControllerBase
+namespace csharp_crud_api.Controllers
 {
-  private readonly UserContext _context;
-
-  public UsersController(UserContext context)
-  {
-    _context = context;
-  }
-
-  // GET: api/users
-  [HttpGet]
-  public async Task<ActionResult<IEnumerable<User>>> GetUsers()
-  {
-    return await _context.Users.ToListAsync();
-  }
-
-  // GET: api/users/5
-  [HttpGet("{id}")]
-  public async Task<ActionResult<User>> GetUser(int id)
-  {
-    var user = await _context.Users.FindAsync(id);
-
-    if (user == null)
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
     {
-      return NotFound();
+        private readonly UserContext _context;
+
+        public UsersController(UserContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/users
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        // GET: api/users/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetUser(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return user;
+        }
+
+        // POST api/users
+        [HttpPost]
+        public async Task<ActionResult<User>> PostUser(User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetUser), new { id = user.Id }, user);
+        }
+
+        // PUT api/users/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutUser(int id, User user)
+        {
+            if (id != user.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(user).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        // DELETE api/users/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUser(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            _context.Users.Remove(user);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        // GET: api/users/search
+        [HttpGet("search")]
+        public async Task<ActionResult<IEnumerable<User>>> SearchUsers([FromQuery] string query)
+        {
+            var users = await _context.Users
+                .Where(u => EF.Functions.Like(u.Name, $"%{query}%") || EF.Functions.Like(u.Email, $"%{query}%"))
+                .ToListAsync();
+
+            if (users == null || !users.Any())
+            {
+                return NotFound();
+            }
+
+            return users;
+        }
+
+        // GET: api/users/test
+        [HttpGet("test")]
+        public string Test()
+        {
+            return "Hello World!";
+        }
+
+        // GET: api/users/movies
+        [HttpGet("movies")]
+        public async Task<ActionResult<IEnumerable<Movie>>> GetMovies()
+        {
+            return await _context.Movies.ToListAsync();
+        }
+
+        // GET: api/users/movies/5
+        [HttpGet("movies/{id}")]
+        public async Task<ActionResult<Movie>> GetMovie(int id)
+        {
+            var movie = await _context.Movies.FindAsync(id);
+
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            return movie;
+        }
+
+        // POST api/users/movies
+        [HttpPost("movies")]
+        public async Task<ActionResult<Movie>> PostMovie(Movie movie)
+        {
+            _context.Movies.Add(movie);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetMovie), new { id = movie.Id }, movie);
+        }
+
+        // PUT api/users/movies/5
+        [HttpPut("movies/{id}")]
+        public async Task<IActionResult> PutMovie(int id, Movie movie)
+        {
+            if (id != movie.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(movie).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        // DELETE api/users/movies/5
+        [HttpDelete("movies/{id}")]
+        public async Task<IActionResult> DeleteMovie(int id)
+        {
+            var movie = await _context.Movies.FindAsync(id);
+
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            _context.Movies.Remove(movie);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
     }
-
-    return user;
-  }
-
-  // POST api/users
-  [HttpPost]
-  public async Task<ActionResult<User>> PostUser(User user)
-  {
-    _context.Users.Add(user);
-    await _context.SaveChangesAsync();
-
-    return CreatedAtAction(nameof(GetUser), new { id = user.Id }, user);
-  }
-
-  // PUT api/users/5
-  [HttpPut("{id}")]
-  public async Task<IActionResult> PutUser(int id, User user)
-  {
-    if (id != user.Id)
-    {
-      return BadRequest();
-    }
-
-    _context.Entry(user).State = EntityState.Modified;
-    await _context.SaveChangesAsync();
-
-    return NoContent();
-  }
-
-  // DELETE api/users/5
-  [HttpDelete("{id}")]
-  public async Task<IActionResult> DeleteUser(int id)
-  {
-    var user = await _context.Users.FindAsync(id);
-
-    if (user == null)
-    {
-      return NotFound();
-    }
-
-    _context.Users.Remove(user);
-    await _context.SaveChangesAsync();
-
-    return NoContent();
-  }
-  [HttpGet("search")]
-  public async Task<ActionResult<IEnumerable<User>>> SearchUsers([FromQuery] string query)
-  {
-    var users = await _context.Users
-        .Where(u => EF.Functions.Like(u.Name, $"%{query}%") || EF.Functions.Like(u.Email, $"%{query}%"))
-        .ToListAsync();
-
-    if (users == null || !users.Any())
-    {
-        return NotFound();
-    }
-
-    return users;
-  }
-  // dummy endpoint to test the database connection
-  [HttpGet("test")]
-  public string Test()
-  {
-    return "Hello World!";
-  }
 }
