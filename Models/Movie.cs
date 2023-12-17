1cba3e1e3f7f82b981e31060c907b052fc540bc2
Anthony-Suni <85529514+Anthony-Suni@users.noreply.github.com>
2023-12-17 15:23:02 -0500

    Update Movie.cs

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -1,1 +1,22 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Models
+{
+    [Table("movies")]
+    public class Movie
+    {
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Column("pelicula")]
+        public string Pelicula { get; set; }
+
+        [Column("genero")]
+        public string Genero { get; set; }
+
+        // Agregado: Este atributo indica que la columna 'ratings' es un array en la base de datos
+        [Column("ratings", TypeName = "integer[]")] 
+        public int[] Ratings { get; set; }
+    }
+}
 
