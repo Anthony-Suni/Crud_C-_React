ad07ec2fa08fda74b2256a1dde77820077139f19
Anthony-Suni <85529514+Anthony-Suni@users.noreply.github.com>
2023-12-17 17:41:41 -0500

    Update UserContext.cs

--- Data/UserContext.cs
+++ Data/UserContext.cs
@@ -3,13 +3,12 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Data
 {
-    public class UserContext : DbContext
+  public class UserContext : DbContext
+  {
+    public UserContext(DbContextOptions<UserContext> options) : base(options)
     {
-        public UserContext(DbContextOptions<UserContext> options) : base(options)
-        {
-        }
-
-        public DbSet<User> Users { get; set; }
-        public DbSet<Movie> Movies { get; set; } 
     }
+
+    public DbSet<User> Users { get; set; }
+  }
 }
