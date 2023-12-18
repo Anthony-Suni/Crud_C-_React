9dd45443c0ce6a13984d08b5d74fbf662dfb539b
Anthony-Suni <85529514+Anthony-Suni@users.noreply.github.com>
2023-12-17 21:52:33 -0500

    Update RatingContext.cs

--- Data/RatingContext.cs
+++ Data/RatingContext.cs
@@ -1,1 +1,23 @@
-lal
+using Microsoft.EntityFrameworkCore;
+using Models;
+
+namespace Data
+{
+    public class RatingContext : DbContext
+    {
+        public RatingContext(DbContextOptions<RatingContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Rating> Ratings { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Configuraciones específicas de MovieContext si es necesario
+            // ...
+
+            modelBuilder.Entity<Rating>()
+                .HasKey(r => new { r.UserId, r.MovieId }); // Definir clave primaria compuesta
+        }
+    }
+}
