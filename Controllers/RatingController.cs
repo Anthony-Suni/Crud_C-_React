c4f82497925812da3fe6a90dd813aafd6c225408
Anthony-Suni <85529514+Anthony-Suni@users.noreply.github.com>
2023-12-17 22:20:28 -0500

    Update RatingController.cs

--- Controllers/RatingController.cs
+++ Controllers/RatingController.cs
@@ -1,7 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Models;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -41,48 +40,6 @@ namespace csharp_crud_api.Controllers
             return Ok(rating);
         }
 
-        // POST api/ratings
-        [HttpPost]
-        public async Task<ActionResult<Rating>> PostRating(Rating rating)
-        {
-            _context.Ratings.Add(rating);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction(nameof(GetRating), new { userId = rating.UserId, movieId = rating.MovieId }, rating);
-        }
-
-        // PUT api/ratings/5
-        [HttpPut("{userId}/{movieId}")]
-        public async Task<IActionResult> PutRating(int userId, int movieId, Rating rating)
-        {
-            if (userId != rating.UserId || movieId != rating.MovieId)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(rating).State = EntityState.Modified;
-            await _context.SaveChangesAsync();
-
-            return NoContent();
-        }
-
-        // DELETE api/ratings/5
-        [HttpDelete("{userId}/{movieId}")]
-        public async Task<IActionResult> DeleteRating(int userId, int movieId)
-        {
-            var rating = await _context.Ratings.FindAsync(userId, movieId);
-
-            if (rating == null)
-            {
-                return NotFound();
-            }
-
-            _context.Ratings.Remove(rating);
-            await _context.SaveChangesAsync();
-
-            return NoContent();
-        }
-
         // GET: api/ratings/user/5
         [HttpGet("user/{userId}")]
         public ActionResult<IEnumerable<Rating>> GetRatingsByUser(int userId)
@@ -112,4 +69,5 @@ namespace csharp_crud_api.Controllers
         }
     }
 }
+
 
