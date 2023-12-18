1a8a39e33ba1551e30672e365235bc26d00f26a1
Anthony-Suni <85529514+Anthony-Suni@users.noreply.github.com>
2023-12-17 22:40:54 -0500

    Update RatingsController.cs

--- Controllers/RatingsController.cs
+++ Controllers/RatingsController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Models;
+using Data;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
