1196933491bab67165e784a363db68f7bfb3963d
Anthony-Suni <85529514+Anthony-Suni@users.noreply.github.com>
2023-12-17 23:04:30 -0500

    Update test.sql

--- test.sql
+++ test.sql
@@ -22,10 +22,15 @@ CREATE TABLE movies (
 
 
 -- Crear la tabla 'movies'
-CREATE TABLE movies (
-    movieId INT PRIMARY KEY,
-    title VARCHAR(255),
-    genres VARCHAR(255) NULL
+CREATE TABLE ratings (
+    userId INT,
+    movieId INT,
+    rating FLOAT,
+    timestamp INT NULL,
+    PRIMARY KEY (userId, movieId),
+    FOREIGN KEY (userId) REFERENCES users(userId),
+    FOREIGN KEY (movieId) REFERENCES movies(movieId)
 );
+
 
 -- Puedes agregar más comandos SQL según sea necesario para tu aplicación.
