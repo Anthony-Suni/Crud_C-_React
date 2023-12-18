c40d7ec769b464d83573519921655c0bca612bbc
Anthony-Suni <85529514+Anthony-Suni@users.noreply.github.com>
2023-12-17 23:57:34 -0500

    Update UsersController.cs

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -1,88 +1,94 @@
-using Data;
 using Microsoft.AspNetCore.Mvc;
-using Models;
 using Microsoft.EntityFrameworkCore;
-
-namespace csharp_crud_api.Controllers;
+using Models;
+using Data;
 
 [ApiController]
 [Route("api/[controller]")]
 public class UsersController : ControllerBase
 {
-  private readonly UserContext _context;
+    private readonly UserContext _context;
 
-  public UsersController(UserContext context)
-  {
-    _context = context;
-  }
-
-  // GET: api/users
-  [HttpGet]
-  public async Task<ActionResult<IEnumerable<User>>> GetUsers()
-  {
-    return await _context.Users.ToListAsync();
-  }
-
-  // GET: api/users/5
-  [HttpGet("{id}")]
-  public async Task<ActionResult<User>> GetUser(int id)
-  {
-    var user = await _context.Users.FindAsync(id);
-
-    if (user == null)
+    public UsersController(UserContext context)
     {
-      return NotFound();
+        _context = context;
     }
 
-    return user;
-  }
-
-  // POST api/users
-  [HttpPost]
-  public async Task<ActionResult<User>> PostUser(User user)
-  {
-    _context.Users.Add(user);
-    await _context.SaveChangesAsync();
-
-    return CreatedAtAction(nameof(GetUser), new { id = user.Id }, user);
-  }
-
-  // PUT api/users/5
-  [HttpPut("{id}")]
-  public async Task<IActionResult> PutUser(int id, User user)
-  {
-    if (id != user.Id)
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<User>>> GetUsers()
     {
-      return BadRequest();
+        return await _context.Users.ToListAsync();
     }
 
-    _context.Entry(user).State = EntityState.Modified;
-    await _context.SaveChangesAsync();
-
-    return NoContent();
-  }
-
-  // DELETE api/users/5
-  [HttpDelete("{id}")]
-  public async Task<IActionResult> DeleteUser(int id)
-  {
-    var user = await _context.Users.FindAsync(id);
-
-    if (user == null)
+    [HttpGet("{id}")]
+    public async Task<ActionResult<User>> GetUser(int id)
     {
-      return NotFound();
+        var user = await _context.Users.FindAsync(id);
+
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+        return user;
     }
 
-    _context.Users.Remove(user);
-    await _context.SaveChangesAsync();
+    [HttpPost]
+    public async Task<ActionResult<User>> PostUser(User user)
+    {
+        _context.Users.Add(user);
+        await _context.SaveChangesAsync();
 
-    return NoContent();
-  }
+        return CreatedAtAction(nameof(GetUser), new { id = user.Id }, user);
+    }
 
-  // dummy endpoint to test the database connection
-  [HttpGet("test")]
-  public string Test()
-  {
-    return "Hello World!";
-  }
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutUser(int id, User user)
+    {
+        if (id != user.Id)
+        {
+            return BadRequest();
+        }
+
+        _context.Entry(user).State = EntityState.Modified;
+
+        try
+        {
+            await _context.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+            if (!UserExists(id))
+            {
+                return NotFound();
+            }
+            else
+            {
+                throw;
+            }
+        }
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteUser(int id)
+    {
+        var user = await _context.Users.FindAsync(id);
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+        _context.Users.Remove(user);
+        await _context.SaveChangesAsync();
+
+        return NoContent();
+    }
+
+    private bool UserExists(int id)
+    {
+        return _context.Users.Any(e => e.Id == id);
+    }
 }
+
