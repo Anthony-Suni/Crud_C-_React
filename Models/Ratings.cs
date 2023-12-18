2fa0b847a2310c446a3aeea3a487a1b25a4389b0
Anthony-Suni <85529514+Anthony-Suni@users.noreply.github.com>
2023-12-18 01:42:55 -0500

    Update Ratings.cs

--- Models/Ratings.cs
+++ Models/Ratings.cs
@@ -5,23 +5,23 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace Models
 {
     [Table("ratings")]
-    public class Rating
-    {
-        [Key]
-        [Column("userid")]
-        public int UserId { get; set; }
+public class Rating
+{
+    [Key]
+    [Column("userid")]
+    public int UserId { get; set; }
 
-        [Key]
-        [Column("movieid")]
-        public int MovieId { get; set; }
+    [Key]
+    [Column("movieid")]
+    public int MovieId { get; set; }
 
-        [Column("rating")]
-        public int RatingValue { get; set; }
+    [Column("rating")]
+    public int RatingValue { get; set; }
 
-        [Column("timestamp")]
-        public DateTime? Timestamp { get; set; }
+    [Column("timestamp")]
+    public DateTime? Timestamp { get; set; }
 
-        [NotMapped]
-        public int Id => MovieId;
-    }
+    [NotMapped]
+    public int Id => MovieId;
+}
 }
